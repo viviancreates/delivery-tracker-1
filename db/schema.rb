698cb6e1fbcd193db82e110aa29ac016cea8cb95
Vivian Davila <168599473+viviancreates@users.noreply.github.com>
2024-10-07 19:26:22 +0000

    Fix columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_07_162013) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_07_192308) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "deliveries", force: :cascade do |t|
-    t.integer "delivery_id"
     t.string "description"
     t.date "supposed_to_arrive_on"
     t.string "details"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "arrived"
   end
 
   create_table "users", force: :cascade do |t|
